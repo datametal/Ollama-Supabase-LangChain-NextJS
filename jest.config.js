3da2d04befeba8454209a19751c37f183d650e7f
Daryl <daryl@datametal.io>
2024-01-30 20:30:52 -0500

    feat(repo): initial repo setup
    setup project configuration

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+const nextJest = require('next/jest')
+ 
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: './' })
+ 
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom'
+}
+ 
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig)npm i @testing-library/react @testing-library/jest-dom --save-dev
